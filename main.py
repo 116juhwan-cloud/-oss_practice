47e011481b0761c65fe7d21813708f5e1e78a7d9
gigi <gigi@naver.com>
2026-04-20 10:27:23 +0900

    make cal

---
+++ main.py
@@ -0,0 +1,19 @@
+print("A:")
+
+A = int(input())
+print("A:")
+
+B = int(input())
+print("B:")
+
+print("+-*/")
+op = input()
+
+if op == "+":
+    print(A+B)
+elif op == "-":
+    print(A-B)
+elif op == "*":
+    print(A*B)
+else
+    print(A/B)
